df98036ccbb840e7c28645efec20993d08f37273
Vinay0022 <vinu031995@gamil.com>
2022-07-01 01:12:11 +0530

    Added Login User UI

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,14 +2,42 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+//    UI views
+private EditText emailET,passwordET;
+private TextView forgotTv,noAccountTv;
+private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailET = findViewById(R.id.emailET);
+        passwordET= findViewById(R.id.passwordET);
+        forgotTv= findViewById(R.id.forgotTv);
+        noAccountTv= findViewById(R.id.noAccountTv);
+        loginBtn= findViewById(R.id.loginBtn);
+
+        noAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+           startActivity(new Intent(LoginActivity.this,RegisterUserActivity.class));
+            }
+        });
+
+forgotTv.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+            startActivity(new Intent(LoginActivity.this,ForgotPasswordActivity.class));
+    }
+});
 
 
     }
---
+++ app/src/main/java/com/example/myapplication/RegisterUserActivity.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegisterUserActivity extends AppCompatActivity {
+    private ImageButton backBtn,gpsBtn;
+    private ImageView profileIv;
+    private EditText nameET,phoneET,passwordET,cPasswordET,countryET,stateET,cityET,addressET,emailET;
+    private Button registerBtn;
+    private TextView registerSellerTv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+        // init ui views
+        backBtn = findViewById(R.id.backBtn);
+        gpsBtn= findViewById(R.id.gpsBtn);
+        profileIv =findViewById(R.id.profileIv);
+        nameET= findViewById(R.id.nameET);
+        phoneET= findViewById(R.id.phoneET);
+        countryET= findViewById(R.id.countryET);
+        stateET= findViewById(R.id.stateET);
+        cityET =findViewById(R.id.cityET);
+        addressET=findViewById(R.id.addressET);
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+        cPasswordET = findViewById(R.id.cPasswordET);
+        registerBtn= findViewById(R.id.recoverBtn);
+        registerSellerTv= findViewById(R.id.registerSellerTv);
+
+backBtn.setOnClickListener(new View.OnClickListener(){
+    @Override
+    public void onClick(View v){
+
+    }
+});
+gpsBtn.setOnClickListener(new View.OnClickListener(){
+    @Override
+    public void onClick(View v){
+        // detect current location
+         }
+        });
+profileIv.setOnClikListener(new View.OnClickListener(){
+    @Override
+    public void onClick(View v){
+    }
+});
+ registerBtn.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View view) {
+//         register User
+     }
+ });
+}
+    }
